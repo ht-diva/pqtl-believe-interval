e13f0cde4397a5259ce6946517200c0de4573042
SoCadiou <solene.cadiou@fht.org>
2023-07-27 15:58:47 +0200

    creating script for comparisons of 2 conditions

---
+++ Comparisons_2_conditions_on_test_targets.R
@@ -0,0 +1,16 @@
+################################################################################
+###Script to compare GWAS results for protein in condition A vs condition B ###
+###############################################################################
+
+####0.loading results####
+###regenie###
+###HT pipe###
+###fastLMM###
+
+##formatting results
+
+####1. inflation####
+
+####2.GWAS signals####
+
+####3. replication of previous studies####
