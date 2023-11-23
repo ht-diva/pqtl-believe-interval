749222e6eb18ff11cf6426397c40c84874bb3689
AlessiaMapelli <94119303+AlessiaMapelli@users.noreply.github.com>
2023-11-23 12:24:40 +0100

    Update ProtPCs-pheno_association.R

--- Prot-pheno_association/ProtPCs-pheno_association.R
+++ Prot-pheno_association/ProtPCs-pheno_association.R
@@ -4,17 +4,26 @@ library(lme4)
 library(dplyr)
 library(ggplot2)
 
-col_of_start_protein <- 95
 
-data_full <- readRDS("/center/healthds/pQTL/INTERVAL/cleaned_INTERVAL.Rds")$imputed_cleaned_dataset
-protnames <- colnames(data_full)[col_of_start_protein:length(colnames(data_full))]
-protdata <- data_full[protnames]
-SampleId <- data_full$SampleId
+file_path <- "/exchange/healthds/pQTL/INTERVAL/Proteomics_QC_files/data_all_not_imputed_without_transform_ANMLSMP_INTERVAL_QC.Rds"
+data_new <- readRDS(file_path)
+head(colnames(data_new), 33)
+tail(colnames(data_new), 62)
+col_of_start_protein <- 34
+col_of_end_protein <- length(colnames(data_new)) - 61
+protnames <- colnames(data_new)[col_of_start_protein:col_of_end_protein]
+protdata <- data_new[protnames]
+SampleId <- data_new$SampleId
 export_prot_data <- cbind(protdata, SampleId)
 write.csv(export_prot_data, "Interval_prot_data.csv")
-phenonames <- colnames(data_full)[1:col_of_start_protein-1]
-phenodata <- data_full[phenonames]
+
+phenonames_1 <- colnames(data_new)[1:col_of_start_protein-1]
+phenodata_1 <- data_new[phenonames_1]
+phenonames_2 <- colnames(data_new)[(col_of_end_protein+1): length(colnames(data_new))]
+phenodata_2 <- data_new[phenonames_2]
+phenodata <- cbind(phenodata_1, phenodata_2)
 agePulse2 <- phenodata$agePulse*phenodata$agePulse
+phenodata$bmi<-as.numeric(phenodata$wt_bl)/(as.numeric(phenodata$ht_bl)^2)
 phenodata <- cbind(phenodata, agePulse2)
 phenodata$processDate_bl <- as.Date(phenodata$processDate_bl, "%d%b%Y")
 phenodata$attendanceDate <- as.Date(phenodata$attendanceDate, "%d%b%Y")
@@ -28,7 +37,6 @@ write.csv(phenodata, "Interval_pheno_data.csv")
 meta_data_full <- readRDS("/center/healthds/pQTL/INTERVAL/cleaned_INTERVAL.Rds")$metadata
 write.csv(meta_data_full, "Interval_meta_data.csv")
 
-
 ####PCA to assess covariates effect####
 ##log transform, scale
 data<-as.data.frame(lapply(protdata,function(x) {scale(log(x))}))
@@ -40,7 +48,7 @@ A<-as.matrix(acp_p1$var$coord)
 dt_PC<-as.matrix(dt_PC)
 PC<-dt_PC%*%A
 Pc<-as.data.frame(PC)
-SampleId <- data_full$SampleId
+SampleId <- data_new$SampleId
 Pc <- cbind(Pc, SampleId)
 write.csv(Pc, "PCs_10.csv")
 
@@ -79,15 +87,8 @@ summary(mod)
 mod<-lm(data=dtjoin,formula=Dim.2~difftime)
 summary(mod)
 
-mod<-lm(data=dtjoin,formula=Dim.1~Batch+sexPulse+agePulse+PlateId+bmi+difftime)
+mod<-lm(data=dtjoin,formula=Dim.1~sexPulse+agePulse+PlateId+difftime)
 summary(mod)
-mod<-lm(data=dtjoin,formula=Dim.2~Batch+sexPulse+agePulse+PlateId+bmi+difftime)
+mod<-lm(data=dtjoin,formula=Dim.2~sexPulse+agePulse+PlateId+difftime)
 summary(mod)
 
-
-mod<-lmer(Dim.1~Batch+sexPulse+agePulse+(1|PlateId), data=dtjoin)
-summary(mod)
-mod<-lmer(Dim.1~batch+bmi+sexPulse+agePulse+as.factor(smCurr_bl)+as.numeric(CRP_bl)+(1|PlateId), data=dtjoin)
-summary(mod)
-# mod<-lmer(Dim.1~bmi+sexPulse+agePulse+KidneyDisease+(1|batch/PlateId), data=dtjoin)
-# summary(mod)
